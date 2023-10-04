cca9c6c7fe56e69699b8e9abcf516c8e8a2089e5
GODCarNageOP <yashvharale@gmail.com>
2023-10-04 20:47:56 +0530

    contact page added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -18,41 +18,14 @@ const Navbar = () => {
                 <img src={Img} alt="logo" className="w-12 h-12"/>
               </a>
               <div className="md:hidden">
-                <button
+                <div
                   className="p-2 text-white rounded-md outline-none :border-gray-400 focus:border"
                   onClick={() => setNavbar(!navbar)}
                 >
-                  {navbar ? (
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      className="w-6 h-6 text-white"
-                      viewBox="0 0 20 20"
-                      fill="currentColor"
-                    >
-                      <path
-                        fillRule="evenodd"
-                        d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                        clipRule="evenodd"
-                      />
-                    </svg>
-                  ) : (
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      className="w-6 h-6 text-white"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke="currentColor"
-                      strokeWidth={2}
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="M4 6h16M4 12h16M4 18h16"
-                      />
-                    </svg>
-                  )}
+                  <Hamburger/>
+                  
 
-                </button>
+                </div>
               </div>
             </div>
           </div>
@@ -62,22 +35,19 @@ const Navbar = () => {
                 }`}
             >
               <ul className="items-center justify-center space-y-8 md:flex md:space-x-6 md:space-y-0 text-base uppercase">
-                <li className="text-white hover:text-indigo-200 px-3 py-2 bg-[#202020] rounded-2xl">
+                <li className="text-white hover:opacity-80 px-3 py-2 bg-[#202020] rounded-2xl">
                   <Link to="/">Home</Link>
                 </li>
-                <li className="text-white hover:text-indigo-200  px-3 py-2 bg-[#202020] rounded-2xl">
+                <li className="text-white hover:opacity-80  px-3 py-2 bg-[#202020] rounded-2xl">
                   <Link to="/products">Products</Link>
                 </li>
-                <li className="text-white hover:text-indigo-200  px-3 py-2 bg-[#202020] rounded-2xl">
+                <li className="text-white hover:opacity-80  px-3 py-2 bg-[#202020] rounded-2xl">
                   <Link to="/aboutus">About</Link>
                 </li>
-                <li className="text-white hover:text-indigo-200  px-3 py-2 bg-[#202020] rounded-2xl">
+                <li className="text-white hover:opacity-80  px-3 py-2 bg-[#202020] rounded-2xl">
                   <Link to="/contactus">Contact</Link>
                 </li>
               </ul>
-              {/* <div className="mt-3 space-y-2 lg:hidden md:inline-block border-white rounded-md " onClick={handleClick}>
-                <Hamburger size={22} color="white" />
-              </div> */}
             </div>
           </div>
         </div>
--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Product = () => {
   return (
-    <div className='w-full h-screen'>Product</div>
+    <div className='w-full h-screen bg-gray-100'>Product</div>
   )
 }
 
